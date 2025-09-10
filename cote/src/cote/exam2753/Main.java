5e501bea4a9a85ea0b6708aacd54329dffedb90e
SiheonKim <ksh20090512@gmail.com>
2025-09-10 11:48:04 +0900

    01

--- cote/src/cote/exam2753/Main.java
+++ cote/src/cote/exam2753/Main.java
@@ -1,10 +1,21 @@
 package cote.exam2753;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+				Scanner a = new Scanner(System.in);
+				int A = a.nextInt();
+				
+				a.close();
+				
+				if(A%4==0) {
+					if(A%400==0) System.out.println("1");
+					else if(A%100==0) System.out.println("0");
+					else System.out.println("1");
+				}
+				else System.out.println("0");
 	}
 
 }
