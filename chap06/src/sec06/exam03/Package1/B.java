64b31ded0725894903158bc63718f471aa36e82a
SiheonKim <ksh20090512@gmail.com>
2025-09-22 12:28:59 +0900

    01

---
+++ chap06/src/sec06/exam03/Package1/B.java
@@ -0,0 +1,5 @@
+package sec06.exam03.Package1;
+
+public class B {
+	A a;
+}
---
+++ chap06/src/sec06/exam03/Package2/C.java
@@ -0,0 +1,8 @@
+package sec06.exam03.Package2;
+
+import sec06.exam03.Package1.*;
+
+public class C {
+//	A a;
+	B b;
+}
